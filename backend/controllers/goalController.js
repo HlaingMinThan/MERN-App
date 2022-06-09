d812b0cd3d27b2e501d65bebf24accf8e3884ec3
HlaingMinThan <hlaingminthan@92@gmail.com>
2022-06-10 01:04:43 +0700

    formal handle error way with async

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -31,21 +31,29 @@ let goalController={
      *@access Private
      */
     async updateGoals(req,res){
-        if(!req.body.title){
-            res.status(400);//set custom status
-            throw new Error('Please add the title field');
+        try{
+            if(!req.body.title){
+                res.status(400);//set custom status
+                throw new Error('Please add the title field');
+            }
+    
+            let goal=await Goal.findById(req.params.id);
+            if(!goal){
+                res.status(400);//set custom status
+                throw new Error('Goal not exists with that id');
+            }
+            
+            let updatedGoal=await Goal.findByIdAndUpdate(req.params.id,req.body,{
+                new:true//will give the updated value
+            });
+            res.status(200).json(updatedGoal);
+        }catch(e){
+            res.status(500)//check the status is already defined or not
+            .json({
+                message:e.message,
+                stack:process.env.APP_ENV==='local' ? e.stack : null
+            })
         }
-
-        let goal=await Goal.findById(req.params.id);
-        if(!goal){
-            res.status(400);//set custom status
-            throw new Error('Goal not exists with that id');
-        }
-        
-        let updatedGoal=await Goal.findByIdAndUpdate(req.params.id,req.body,{
-            new:true//will give the updated value
-        });
-        res.status(200).json(updatedGoal);
     },
     /**
      *@desc  Delete goals
