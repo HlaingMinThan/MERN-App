a3afad8b973ec089c940b56bde3dfaf59a6cdecd
HlaingMinThan <hlaingminthan@92@gmail.com>
2022-06-10 01:05:39 +0700

    solve try and catch with asyncHandler

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,4 +1,5 @@
 const Goal=require('../Models/Goal')
+const asyncHandler=require('express-async-handler');
 
 let goalController={
     /**
@@ -6,16 +7,16 @@ let goalController={
      *@route GET /api/goals
      *@access Private
      */
-    async getGoals(req,res){
+     getGoals:asyncHandler(async(req,res)=>{
         let goals=await Goal.find();
         res.status(200).json(goals);
-    },
+    }),
      /**
      *@desc  Set goals
      *@route POST /api/goals
      *@access Private
      */
-    async setGoals(req,res){
+     setGoals:asyncHandler(async(req,res)=>{
         if(!req.body.title){
             res.status(400);//set custom status
             throw new Error('Please add the title field');
@@ -24,43 +25,35 @@ let goalController={
             title:req.body.title
         })
         res.status(200).json(goal);
-    },
+    }),
      /**
      *@desc  Update goals
      *@route PUT /api/goals/:id
      *@access Private
      */
-    async updateGoals(req,res){
-        try{
-            if(!req.body.title){
-                res.status(400);//set custom status
-                throw new Error('Please add the title field');
-            }
-    
-            let goal=await Goal.findById(req.params.id);
-            if(!goal){
-                res.status(400);//set custom status
-                throw new Error('Goal not exists with that id');
-            }
-            
-            let updatedGoal=await Goal.findByIdAndUpdate(req.params.id,req.body,{
-                new:true//will give the updated value
-            });
-            res.status(200).json(updatedGoal);
-        }catch(e){
-            res.status(500)//check the status is already defined or not
-            .json({
-                message:e.message,
-                stack:process.env.APP_ENV==='local' ? e.stack : null
-            })
+     updateGoals:asyncHandler(async(req,res)=>{
+        if(!req.body.title){
+            res.status(400);//set custom status
+            throw new Error('Please add the title field');
         }
-    },
+
+        let goal=await Goal.findById(req.params.id);
+        if(!goal){
+            res.status(400);//set custom status
+            throw new Error('Goal not exists with that id');
+        }
+        
+        let updatedGoal=await Goal.findByIdAndUpdate(req.params.id,req.body,{
+            new:true//will give the updated value
+        });
+        res.status(200).json(updatedGoal);
+    }),
     /**
      *@desc  Delete goals
      *@route DELETE /api/goals/:id
      *@access Private
      */
-    async deleteGoals(req,res){
+     deleteGoals:asyncHandler(async (req,res)=>{
         let goal=await Goal.findById(req.params.id);
         if(!goal){
             res.status(400);//set custom status
@@ -70,7 +63,7 @@ let goalController={
         res.status(200).json({
             id:req.params.id
         });
-    }
+    })
 }
 
 module.exports=goalController;
