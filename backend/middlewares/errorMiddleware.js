85a8a742afd72a9ee44cadaf27fac783fe78c3e8
HlaingMinThan <hlaingminthan@92@gmail.com>
2022-06-09 17:51:05 +0700

    add comment

--- backend/middlewares/errorMiddleware.js
+++ backend/middlewares/errorMiddleware.js
@@ -1,5 +1,5 @@
 const errorHandler=(err,req,res,next)=>{
-    res.status(res.statusCode ?? 500)
+    res.status(res.statusCode ?? 500)//check the status is already defined or not
         .json({
             message:err.message,
             stack:process.env.APP_ENV==='local' ? err.stack : null
