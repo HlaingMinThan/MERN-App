398ee60902892c120ec58363c3a9b8ebdc5d8c1a
HlaingMinThan <hlaingminthan@92@gmail.com>
2022-06-09 17:02:11 +0700

    set up basic express app with route module splitting

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,8 @@
-console.log('server');
+require('dotenv').config()
+const PORT=process.env.PORT||8000;
+const express=require("express");
+const app=express();
+
+app.use('/api/goals',require('./routes/goalRoutes.js'))
+
+app.listen(PORT,()=>console.log(`Server is running on ${PORT}`))
