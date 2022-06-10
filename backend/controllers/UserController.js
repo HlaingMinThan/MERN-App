a561f1bf1989d9d4deed7e1c88a3ed8ae2f153a9
HlaingMinThan <hlaingminthan@92@gmail.com>
2022-06-10 14:56:57 +0700

    register user api done

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,4 +1,5 @@
 const User = require("../Models/User");
+const bcrypt=require('bcryptjs');
 const asyncHandler = require("express-async-handler");
 
 let UserController = {
@@ -27,7 +28,21 @@ let UserController = {
    *@access Public
    */
   register: asyncHandler(async (req, res) => {
-    res.json({msg:'Register user'})
+    let {name,email,password}=req.body;
+    if(!name || !email || !password){
+      res.status(400);
+      throw new Error('please add all fields');
+    }
+    let user=await User.findOne({email});
+    if(user){
+      res.status(400);
+      throw new Error('User already exists');
+    }
+    let salt=await bcrypt.genSalt(10);
+    let hashPW=await bcrypt.hash(password,salt);
+    let newUser=await User.create({name,email,password:hashPW});
+
+    res.json({name,email,_id:newUser._id})
   }),
 
 };
