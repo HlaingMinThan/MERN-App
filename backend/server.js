d50cb37f007256bf69ed6fe73ffc7353066b5ac6
HlaingMinThan <hlaingminthan@92@gmail.com>
2022-06-10 15:38:02 +0700

    fix name

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
 app.use('/api/goals',require('./routes/goalRoutes.js'))
-app.use('/api/users',require('./routes/getUsers.js'))
+app.use('/api/users',require('./routes/userRoutes.js'))
 
 //error will throw from route controller so need to catch after that
 app.use(errorHandler);
